a239a52a92bf79ff813ebc4e1299c49a3c590475
Erameh Michael <eramehmichael2000@gmail.com>
2024-07-09 06:51:30 +0100

    livvechat

--- trade/dashboard/auth/register.php
+++ trade/dashboard/auth/register.php
@@ -69,10 +69,10 @@ $message ="";
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'mail.pipradariinvest.org';                     //Set the SMTP server to send through
+    $mail->Host       = 'mail.dreamhost.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'support@pipradariinvest.org';                     //SMTP username
-    $mail->Password   = 'trading12345@67';                               //SMTP password
+    $mail->Password   = 'trading12345@67!';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
